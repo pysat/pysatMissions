9c438c32a9c961efe106349769c42c6641e0e2b0
Jeff Klenzing <jklenzing@gmail.com>
2019-10-22 22:24:59 -0400

    ENH: add version

--- pysatMissionPlanning/__init__.py
+++ pysatMissionPlanning/__init__.py
@@ -4,3 +4,8 @@ from __future__ import absolute_import
 from . import instruments
 
 __all__ = ['instruments']
+
+# set version
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'version.txt')) as version_file:
+    __version__ = version_file.read().strip()
