e4c5cedbef2612845f0d920e1a0f2529e3b8ffa1
Jeff Klenzing <jklenzing@gmail.com>
2019-10-22 22:59:42 -0400

    BUG: import in init

--- pysatMissionPlanning/instruments/__init__.py
+++ pysatMissionPlanning/instruments/__init__.py
@@ -3,6 +3,6 @@ pysatMissionPlanning.instruments is a pysat module that provides
 the instrument modules to be used with pysat
 """
 
-from instruments import pysat_sgp4
+from pysatMissionPlanning.instruments import pysat_sgp4
 
 __all__ = ['pysat_sgp4']
--- pysatMissionPlanning/methods/__init__.py
+++ pysatMissionPlanning/methods/__init__.py
@@ -3,6 +3,6 @@ pysatMissionPlanning.methods is a module that provides
 the methods to interface with numerous empirical model packages
 """
 
-from methods import aacgmv2, apexpy, pyglow
+from pysatMissionPlanning.methods import aacgmv2, apexpy, pyglow
 
 __all__ = ['aacgmv2', 'apexpy', 'pyglow']
