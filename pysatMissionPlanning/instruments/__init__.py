722eaac3f7a99d0d1d516e06d047a466fcc572ed
Jeff Klenzing <jklenzing@gmail.com>
2019-10-22 22:52:56 -0400

    STY: init import

--- pysatMissionPlanning/instruments/__init__.py
+++ pysatMissionPlanning/instruments/__init__.py
@@ -3,6 +3,6 @@ pysatMissionPlanning.instruments is a pysat module that provides
 the instrument modules to be used with pysat
 """
 
-from . import pysat_sgp4
+from instruments import pysat_sgp4
 
 __all__ = ['pysat_sgp4']
