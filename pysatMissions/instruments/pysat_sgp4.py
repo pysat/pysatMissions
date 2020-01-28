4397aae62420dc73074e4da012001683e4141f78
Jeff Klenzing <jklenzing@gmail.com>
2020-01-28 16:19:57 -0500

    BUG: typo

--- pysatMissions/instruments/pysat_sgp4.py
+++ pysatMissions/instruments/pysat_sgp4.py
@@ -12,7 +12,7 @@ import os
 import functools
 import pandas as pds
 import pysat
-from pysatMissionPlanning.instruments import _core as mcore
+from pysatMissions.instruments import _core as mcore
 
 # pysat required parameters
 platform = 'pysat'
