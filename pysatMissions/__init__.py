0679e8b3f380457cc64adc95368a6af0650da457
Jeff Klenzing <jklenzing@gmail.com>
2020-03-07 18:19:08 -0500

    ENH: add try/except to init for readthedocs

--- pysatMissions/__init__.py
+++ pysatMissions/__init__.py
@@ -1,10 +1,14 @@
 from __future__ import print_function
 from __future__ import absolute_import
-
+import logging
 import os
-from pysatMissions import instruments
-from pysatMissions import methods
-from pysatMissions import plot
+
+try:
+    from pysatMissions import instruments
+    from pysatMissions import methods
+    from pysatMissions import plot
+except ImportError as errstr:
+    logging.exception('problem importing pysatMissions: ' + str(errstr))
 
 __all__ = ['instruments', 'methods', 'plot']
 
