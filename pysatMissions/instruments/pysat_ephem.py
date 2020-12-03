02688fbe375a5f17d446ecfe06bf96df7c5c933f
Jeff Klenzing <jklenzing@gmail.com>
2020-12-03 14:31:46 -0500

    STY: indentation

--- pysatMissions/instruments/pysat_ephem.py
+++ pysatMissions/instruments/pysat_ephem.py
@@ -141,8 +141,8 @@ def load(fnames, tag=None, inst_id=None, obs_long=0., obs_lat=0., obs_alt=0.,
         lp['alt'] = sat.elevation / 1000.
         # get ECEF position of satellite
         lp['x'], lp['y'], lp['z'] = pysatMagVect.geodetic_to_ecef(lp['glat'],
-                                                           lp['glong'],
-                                                           lp['alt'])
+                                                                  lp['glong'],
+                                                                  lp['alt'])
         output_params.append(lp)
 
     output = pds.DataFrame(output_params, index=times)
