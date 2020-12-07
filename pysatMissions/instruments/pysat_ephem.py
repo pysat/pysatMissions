aa5d0e067bb06066dbadb0462e5cea3082feeaf8
Russell Stoneback <rstoneba@utdallas.edu>
2020-12-07 16:54:45 -0600

    ENH: Updated to latest pysat custom method signature

--- pysatMissions/instruments/pysat_ephem.py
+++ pysatMissions/instruments/pysat_ephem.py
@@ -43,10 +43,10 @@ def init(self):
 
     """
 
-    self.custom.attach(mm_magcoord.add_quasi_dipole_coordinates)
-    self.custom.attach(mm_magcoord.add_aacgm_coordinates)
-    self.custom.attach(mm_sc.calculate_ecef_velocity)
-    self.custom.attach(mm_sc.add_ram_pointing_sc_attitude_vectors)
+    self.custom_attach(mm_magcoord.add_quasi_dipole_coordinates)
+    self.custom_attach(mm_magcoord.add_aacgm_coordinates)
+    self.custom_attach(mm_sc.calculate_ecef_velocity)
+    self.custom_attach(mm_sc.add_ram_pointing_sc_attitude_vectors)
 
 
 def load(fnames, tag=None, inst_id=None, obs_long=0., obs_lat=0., obs_alt=0.,
