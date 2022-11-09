f17afd48af7b5aaaaf34675b5f889b59606c81b4
Jeff Klenzing <jklenzing@users.noreply.github.com>
2022-11-09 16:48:52 -0500

    STY: backwards compat

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2021, Authors
+# Full license can be found in License.md
+# -----------------------------------------------------------------------------
+"""Setup routines for pysatMissions."""
+
+from setuptools import setup
+
+# package metadata stored in pyproject.toml
+# tool configurations stored in setup.cfg
+
+setup()
