4f83ad8d801a3f4006e69ae789486cf709f78e7e
Angeline Burrell <angeline.burrell@nrl.navy.mil>
2024-03-29 16:31:18 -0400

    DOC: updated methods headers
    Updated the methods headers with the NRL pub release statement and improved the docstrings.

--- pysatMissions/methods/spacecraft.py
+++ pysatMissions/methods/spacecraft.py
@@ -1,3 +1,9 @@
+# Full author list can be found in .zenodo.json file
+# DOI:10.5281/zenodo.3475498
+#
+# DISTRIBUTION STATEMENT A: Approved for public release. Distribution is
+# unlimited.
+# ----------------------------------------------------------------------------
 """Default routines for projecting values onto vectors for pysat instruments."""
 
 import numpy as np
